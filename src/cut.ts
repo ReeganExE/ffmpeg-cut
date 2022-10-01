4318f4bc22112a9811d211d0521acb22d1c38655
ReeganExE <dongian.rapclubkhtn@gmail.com>
2022-10-01 22:55:26 +0700

    Cut file

--- src/cut.ts
+++ src/cut.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import fs from 'fs/promises';
 
 import execa from 'execa';
 import chalk from 'chalk';
@@ -14,14 +15,31 @@ interface CutArgs {
 }
 
 export default async function cut({ input, sourceFile, start, to }: CutArgs) {
-  const seeks = [];
+  let seeks = [];
   if (start) {
     seeks.push('-ss', start);
   }
   if (to) {
     seeks.push('-to', to);
   }
-  const si = input.streams.find((s) => s.type === 'Subtitle' && (s.lang === 'vi' || s.lang === 'vie'));
+
+  const file = path.parse(sourceFile);
+  const tempFile = `${file.name}-temp.${file.ext}`;
+  const isRemote = sourceFile.startsWith('http');
+  if (isRemote) {
+    console.error(chalk.yellowBright(`Cutting file ...: ${tempFile}`));
+    // we have to cut it to a local file
+    await execa(
+      'ffmpeg',
+      ['-hide_banner', ...seeks, '-i', sourceFile, '-map', '0', '-map_chapters', '-1', '-c', 'copy', '-y', tempFile],
+      { stderr: 'inherit' }
+    );
+    seeks = [];
+  }
+
+  sourceFile = isRemote ? tempFile : sourceFile;
+  const subStream = input.streams.find((s) => s.type === 'Subtitle' && (s.lang === 'vi' || s.lang === 'vie'));
+  const si = subStream ? subStream.stream_index : 0;
   const app = [
     'ffmpeg',
     '-hide_banner',
@@ -30,7 +48,7 @@ export default async function cut({ input, sourceFile, start, to }: CutArgs) {
     '-i',
     sourceFile,
     '-vf',
-    `subtitles=${sourceFile}:si=${si?.stream_index}:force_style="'Fontsize=24,PrimaryColour=&H0001FBFE&'"`,
+    `subtitles=${sourceFile}:si=${si}:force_style="'Fontsize=24,PrimaryColour=&H0001FBFE&'"`,
     '-c:v',
     'hevc_videotoolbox',
     '-q:v',
@@ -39,10 +57,18 @@ export default async function cut({ input, sourceFile, start, to }: CutArgs) {
     'hvc1',
     '-b:v',
     `${input.bitrate}k`,
-    `${path.parse(sourceFile).name}-cut.mp4`,
+    `${file.name}-cut.${file.ext}`,
     '-y',
   ];
 
   console.error(chalk.yellowBright(app.join(' ')));
-  await execa('ffmpeg', app.slice(1), { stderr: 'inherit' });
+  try {
+    await execa('ffmpeg', app.slice(1), { stderr: 'inherit' });
+  } catch (error) {
+    const err = error as execa.ExecaError;
+    console.error(err.stderr);
+    throw new Error(`ffmpeg error code: ${err.exitCode}`);
+  } finally {
+    isRemote && fs.unlink(tempFile);
+  }
 }
