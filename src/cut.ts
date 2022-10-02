5f9c56794189760023b68399fc689cbbe371bb1d
ReeganExE <dongian.rapclubkhtn@gmail.com>
2022-10-02 10:37:42 +0700

    Handle SIGTERM

--- src/cut.ts
+++ src/cut.ts
@@ -23,23 +23,46 @@ export default async function cut({ input, sourceFile, start, to }: CutArgs) {
     seeks.push('-to', to);
   }
 
+  let child: execa.ExecaChildProcess;
+  process.on('SIGTERM', () => {
+    child.kill('SIGTERM');
+    child.on('close', () => process.exit(1));
+  });
+
   const file = path.parse(sourceFile);
   const tempFile = `${file.name}-temp.${file.ext}`;
   const isRemote = sourceFile.startsWith('http');
   if (isRemote) {
     console.error(chalk.yellowBright(`Cutting file ...: ${tempFile}`));
     // we have to cut it to a local file
-    await execa(
+    child = execa(
       'ffmpeg',
-      ['-hide_banner', ...seeks, '-i', sourceFile, '-map', '0', '-map_chapters', '-1', '-c', 'copy', '-y', tempFile],
+      [
+        '-hide_banner',
+        ...seeks,
+        '-i',
+        sourceFile,
+        '-map',
+        '0',
+        '-map_chapters',
+        '-1',
+        '-c',
+        'copy',
+        '-stats',
+        '-loglevel',
+        'error',
+        '-y',
+        tempFile,
+      ],
       { stderr: 'inherit' }
     );
+    await child;
     seeks = [];
   }
 
   sourceFile = isRemote ? tempFile : sourceFile;
   const subStream = input.streams.find((s) => s.type === 'Subtitle' && (s.lang === 'vi' || s.lang === 'vie'));
-  const si = subStream ? subStream.stream_index : 0;
+  const si = subStream ? `:si=${subStream.stream_index}` : '';
   const app = [
     'ffmpeg',
     '-hide_banner',
@@ -48,7 +71,7 @@ export default async function cut({ input, sourceFile, start, to }: CutArgs) {
     '-i',
     sourceFile,
     '-vf',
-    `subtitles=${sourceFile}:si=${si}:force_style="'Fontsize=24,PrimaryColour=&H0001FBFE&'"`,
+    `subtitles=${sourceFile}${si}:force_style='Fontsize=48,PrimaryColour=&H0001FBFE&'`,
     '-c:v',
     'hevc_videotoolbox',
     '-q:v',
@@ -57,13 +80,18 @@ export default async function cut({ input, sourceFile, start, to }: CutArgs) {
     'hvc1',
     '-b:v',
     `${input.bitrate}k`,
+    '-c:a',
+    'aac',
+    '-stats',
+    '-loglevel',
     `${file.name}-cut.${file.ext}`,
     '-y',
   ];
 
   console.error(chalk.yellowBright(app.join(' ')));
   try {
-    await execa('ffmpeg', app.slice(1), { stderr: 'inherit' });
+    child = execa('ffmpeg', app.slice(1), { stderr: 'inherit' });
+    await child;
   } catch (error) {
     const err = error as execa.ExecaError;
     console.error(err.stderr);
